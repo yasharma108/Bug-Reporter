e18513b4ec1838f3e204b832644af76fe364732f
Yash Sharma <yasharma108@gmail.com>
2026-04-26 18:23:53 +0530

    Configure production backend URL

--- client/js/config.js
+++ client/js/config.js
@@ -1,3 +1,2 @@
-// When deploying, change this string to your new Render backend URL!
-// For example: const BACKEND_URL = "https://my-bug-app-xyz.onrender.com";
-const BACKEND_URL = "http://localhost:5000";
+
+const BACKEND_URL = "https://bug-reporter-backend.onrender.com";
